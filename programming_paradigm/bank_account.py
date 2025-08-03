c171dd03a22b960ea4616c8d7a0e08f40ad78197
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-08-03 20:08:09 +0100

    higher corrections

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        return f"Current Balance: ${self.__account_balance}"
 
         
           
