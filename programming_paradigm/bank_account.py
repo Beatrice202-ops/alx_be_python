51a05a68a21057d63d22d8d5bd092fbe23508c90
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-08-03 17:36:40 +0100

    modification in bankaccount file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,12 +3,13 @@ class BankAccount:
         self.account_balance = account_balance
     def deposit(self, amount):
         self.account_balance += amount
+        print(f"deposited: ${amount}")
     def  withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            return True
+            print(f"withdrew: ${amount}")
         else:
-            return False
+            print("insuficient funds")
     def  display_balance(self):
         print(f"current balance: ${self.account_balance}")
 
