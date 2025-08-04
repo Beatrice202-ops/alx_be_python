fe320500eac540313158f7bbf98e72b4d1b2e5ac
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-08-04 16:26:07 +0100

    complex modification

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,11 +5,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
+            print(f"Deposited: ${amount:.1f}")
 
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
+            print(f"Withdrew: ${amount:.1f}")
             return True
+        print("Insufficient funds.")
         return False
 
     def display_balance(self):
